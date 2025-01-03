86c1af79dcb0de0e30177c88d8c9888c8394eceb
Bastianmoral <bastihc@gmail.com>
2025-01-03 14:37:21 -0300

    fixing routes problem

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api/auth";
 
 export const login = async (username, password) => {
   try {
-    const response = await axios.post(`${API_BASE_URL}/auth/login`, {
+    const response = await axios.post(`${API_BASE_URL}/login`, {
       username,
       password,
     });
